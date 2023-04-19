f56da546482b3a24ab73542a1e442221cc320526
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-04-19 18:50:02 +0700

    first

---
+++ QuestDo_MiniProject/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar" style={{backgroundColor: '#5D2E8C'}}>
+        <span className="logo-text" style={{fontFamily: 'Sedan SC'}}>QuestDo</span>
+      <div className="links">
+        <a href="#profile" className="link">Profile</a>
+        <a href="#task" className="link">Task</a>
+        <a href="#setting" className="link">Setting</a>
+        <a href="#about" className="link">About</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
