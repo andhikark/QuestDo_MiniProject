622c0e1b2eb9d6bc0882fdd09d8c8b1bc7628303
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-04-20 14:52:45 +0700

    first

--- QuestDo_MiniProject/src/components/TaskPage.jsx
+++ QuestDo_MiniProject/src/components/TaskPage.jsx
@@ -2,10 +2,35 @@ import React from "react";
 import "../styles/TaskPage.css";
 
 function TaskPage() {
+  const health = 50;
+  const maxHealth = 100;
+  const healthPercentage = (health / maxHealth) * 100;
+
+  const xp = 250;
+  const xpNeeded = 500;
+  const xpPercentage = (xp / xpNeeded) * 100;
+
   return (
     <div className="Container">
         <div className="User-Container">
-            <h1>Task Page</h1>
+          <div className="stats">
+            <img src="https://via.placeholder.com/100x100" alt="Profile" className="profile-pic" />
+            <div className="column">
+              <div className="stat">
+                Health: {health}/{maxHealth}
+                <div className="bar">
+                  <div className="health-bar" style={{ width: `${healthPercentage}%` }}></div>
+                </div>
+              </div>
+              <div className="stat">
+                XP: {xp}/{xpNeeded}
+                <div className="bar">
+                  <div className="xp-bar" style={{ width: `${xpPercentage}%` }}></div>
+                </div>
+              </div>
+              <div className="stat">Level: <span className="value">5</span></div>
+            </div>
+          </div>
         </div>
 
         <div className="Task-Container">
