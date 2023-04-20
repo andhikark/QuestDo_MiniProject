368c91317f6f0ebdd7685eb0b30df6b5eeb0650f
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-04-20 10:00:56 +0700

    first

--- QuestDo_MiniProject/src/App.jsx
+++ QuestDo_MiniProject/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import TaskPage from './components/TaskPage'
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <TaskPage />
     </div>
   )
 }
--- QuestDo_MiniProject/src/components/TaskPage.jsx
+++ QuestDo_MiniProject/src/components/TaskPage.jsx
@@ -1,2 +1,20 @@
 import React from "react";
+import "../styles/TaskPage.css";
 
+function TaskPage() {
+  return (
+    <div className="Container">
+        <div className="User-Container">
+            <h1>Task Page</h1>
+        </div>
+
+        <div className="Task-Container">
+            <h1>Task Page</h1>
+        </div>
+
+    </div>
+    
+  );
+}
+
+export default TaskPage;
