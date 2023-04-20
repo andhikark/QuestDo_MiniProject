dd640d7a118fb791214f9a7b3a6febb786f94098
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-04-20 08:49:25 +0700

    first

--- QuestDo_MiniProject/src/components/Navbar.jsx
+++ QuestDo_MiniProject/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="navbar" style={{backgroundColor: '#5D2E8C'}}>
         <span className="logo-text" style={{fontFamily: 'Sedan SC'}}>QuestDo</span>
-      <div className="links">
+      <div className="links" >
         <a href="#profile" className="link">Profile</a>
         <a href="#task" className="link">Task</a>
         <a href="#setting" className="link">Setting</a>
