fc113a38abd8eb079faa9e9d360b5f5ea0734e29
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-04-24 11:28:48 +0700

    third

--- QuestDo_MiniProject/src/components/SettingsPage.jsx
+++ QuestDo_MiniProject/src/components/SettingsPage.jsx
@@ -3,20 +3,32 @@ import '../styles/SettingsPage.css';
 
 function Settings() {
   return (
-    <div className="settings-container">
-      <div className="profile-picture">
-        <img src="https://www.rainforest-alliance.org/wp-content/uploads/2021/06/capybara-square-1.jpg.optimal.jpg" alt="Profile" width="200" height="200" style={{ borderRadius: "5px" }} />
+    <div>
+      <div className="settings-container">
+        <div className="profile-picture">
+          <img src="https://www.rainforest-alliance.org/wp-content/uploads/2021/06/capybara-square-1.jpg.optimal.jpg" alt="Profile" width="200" height="200" style={{ borderRadius: "5px" }} />
+        </div>
+        <div className="account-info">
+          <div className="email"><p>andhikarestu873@gmail.com</p></div>
+        </div>
       </div>
-      <div className="account-info">
-        <div className="email"><p>andhikarestu873@gmail.com</p></div>
-      </div>
-      <div>
+
+
+      <div className="button-container">
         <button className="my-account-button">
             <span className="button-text">My Account</span>
         </button>
       </div>
 
+      <div className='remainder-container'>
+        
+      </div>
+
+
     </div>
+   
+
+    
   );
 }
 
