aef4866ca49829a4027aaa222cb5331d436831a7
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-05-09 15:52:06 +0700

    fifth

--- QuestDo_MiniProject/src/components/ProfilePage.jsx
+++ QuestDo_MiniProject/src/components/ProfilePage.jsx
@@ -2,26 +2,7 @@ import React from "react";
 import "../styles/ProfilePage.css";
 import UserStats from "./UserStats";
 import NavBar from "./Navbar";
-import Achievement from "./Achievement";
 
-const achievements = [
-  
-  {
-    description: "You have completed your first task!",
-  },
-  {
-    description: "You have completed 5 tasks!",
-  },
-  {
-    description: "You have completed 10 tasks!",
-  },
-  {
-    description: "You have completed 25 tasks!",
-  },
-  {
-    description: "You have completed 50 tasks!",
-  },
-];
 
 function ProfilePage() {
   return (  
@@ -32,11 +13,7 @@ function ProfilePage() {
         <div className="Username">
           <span>CapyBara</span>
         </div>
-          {achievements.map((achievement) => (
-            <Achievement
-              description={achievement.description}
-            />
-          ))}
+          
       </div>
     </div>
   );
