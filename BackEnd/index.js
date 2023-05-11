99a6270633c48675d4c30206c8ce0ee58db79c4c
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-05-11 16:05:36 +0700

    sixth

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,2 +1,31 @@
 const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+const port = 5173;
+
+const connection = mysql.createConnection({
+	host: "server2.bsthun.com",
+	port: "6105",
+	user: "lab_hgnrb",
+	password: "stpj1kjWldx3n6Gn",
+	database: "lab_blank01_h3ceqz",
+});
+
+connection.connect((error) => {
+	if (error) {
+		console.error("Error connecting to database:", error);
+		return;
+	}
+	console.log("Database is connected");
+});
+
+app.use(bodyParser.json({ type: "application/json" }));
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
